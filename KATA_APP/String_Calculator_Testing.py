44b1755debd56c1ab7d3b36c5db32e6020bb69f8
vaishnavi <vaishnavi@dexur.com>
2025-08-03 06:53:17 +0530

    defining empty test class

---
+++ KATA_APP/String_Calculator_Testing.py
@@ -0,0 +1,11 @@
+import unittest
+from String_Calculator import *
+
+class TestStringCalculator(unittest.TestCase):
+
+        def do_nothing():
+            pass
+
+
+if __name__ == "_main_":
+    unittest.main()
